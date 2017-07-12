946ef3799c1a60852b9aa6b555bfd0501763bd11
Pablo Moreno Tepichin <pablomt@MacBook-Pro-de-Pablo.local>
2017-07-12 00:53:47 -0500

    Modified local and production files

--- src/muypicky/settings/local.py
+++ src/muypicky/settings/local.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'zaq0t8+b2a!r*4jk296v_5mt+9_r)#8yeu+i-+-*@pt+reieo4&+5'
+SECRET_KEY = '0t8+b2a!r*4jk296v_5mt+9_r)#8yeu+i-+-*@pt+reieo4&+5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
