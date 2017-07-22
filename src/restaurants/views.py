61b3b891973fe4e8ffa4037a0ca1c221426bfbdb
Pablo Moreno Tepichin <pablomt@MacBook-Pro-de-Pablo.local>
2017-07-21 19:50:02 -0500

    Se utiliza f string, para sustituir strings(nuevo en python 3.6)

--- src/restaurants/views.py
+++ src/restaurants/views.py
@@ -4,6 +4,20 @@ from django.shortcuts import render
 # Create your views here.
 # function based view
 def home(request):
-	return HttpResponse("Hello")
+	html_var = "f strings"
+	html_ = f"""<!DOCTYPE html> 
+	<html lang=es>
+	<head>
+	<body>
+	<h1> Hello World!! </h1>
+	<p> This is {html_var} coming through</p>
+	</body>
+	</head>
+	</html>
+	""" 
+
+	# f strings es nuevo en python 3.6. Es una buena manera de hacer sustitucion de strings
+	# Se necesita poner la f antes de empezar el string con """ para poder realizar la sistitucion
+	return HttpResponse(html_)
 	#return render(request, "template", {})
 
